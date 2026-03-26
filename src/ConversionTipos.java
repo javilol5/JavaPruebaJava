cccde5b1bddc2934c21ba7cb8a500210dab3597c
Casal <javierlolcasal@gmail.com>
2026-03-26 14:52:32 +0100

    calculos de numeros

---
+++ src/ConversionTipos.java
@@ -0,0 +1,13 @@
+public class ConversionTipos {
+    public static void main (String [] args){
+        int numero = -126;
+        byte numPequeno = (byte) numero;
+        int n1 = 55555;
+        int n2 = 66666;
+        long n3 = n1 * n2;
+
+        System.out.println(n3);
+
+        System.out.println(numPequeno);
+    }
+}

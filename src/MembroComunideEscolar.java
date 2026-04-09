45464bf12ca125834706348817bd137c5b88254f
dam <amosqueraalvarez@danielcastelao.org>
2026-04-09 13:16:10 +0200

    feat creacion de MembroCominudadeEscolar.java

---
+++ src/MembroComunideEscolar.java
@@ -0,0 +1,9 @@
+public class MembroComunideEscolar extends Persoa {
+    private int codigoCentro;
+    private String nomeCentro;
+
+    public MembroComunideEscolar (int codCentro, String nomCentro )
+
+
+
+}

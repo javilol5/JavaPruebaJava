3a8952cd1ccfdfb90322ee3726b30fd8d7b5e929
Casal <javierlolcasal@gmail.com>
2026-04-09 12:34:24 +0200

    feat(persoa):
    clase persoa y modificaciones al main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hola mundo");
-    }
+void main() {
+    Persoa p1 = new Persoa();
+    Persoa p2 = new Persoa("Manuel","1234F",45);
+    System.out.println(p1.nome);
+    System.out.println(p2.nome);
 }

f53ee0f2d4cc51315639d8f7eb84ce283eece6e1
Casal <javierlolcasal@gmail.com>
2026-05-28 14:49:36 +0200

    examen1conexion a maquina virtual

---
+++ src/Conexion.java
@@ -0,0 +1,32 @@
+import java.sql.*;
+
+public class Conexion {
+
+    public static void main (String args []) {
+        Connection conexion;
+        Statement sentencia;
+        String sql;
+        String url = "jdbc:postgresql://10.0.8.173:5432/usuarios";
+
+        try {
+            conexion = DriverManager.getConnection(url, "postgres", "postgres");
+            System.out.println("Conectado");
+            String crearTaboaSQL = "CREATE TABLE persoas(nome VARCHAR (50),dni VARCHAR (9),edade INTEGER);";
+            sentencia = conexion.createStatement();
+//            sentencia.execute(crearTaboaSQL);
+//
+//
+//            sentencia.executeUpdate("INSERT INTO persoas(nome, dni, edade)" + "VALUES ('Pepe', '12345678J',23);");
+//            sentencia.executeUpdate("INSERT INTO persoas(nome, dni, edade)" + "VALUES ('Manuel', '23456781P',27);");
+//            sentencia.executeUpdate("INSERT INTO persoas(nome, dni, edade)" + "VALUES ('Daniel', '32145678F',98);");
+//            sentencia.executeUpdate("INSERT INTO persoas(nome, dni, edade)" + "VALUES ('Juan', '87654321J',18);");
+            System.out.println("Datos insertados");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+}
